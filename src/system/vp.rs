76d0c2cb1a66b243d805113156067fd242a089c9
Chris Walker <chris@awsm.tech>
2023-12-31 21:51:01 +0000

    Move RenderSystem into system/ module

---
+++ src/system/vp.rs
@@ -0,0 +1,17 @@
+use nalgebra_glm::identity;
+use nalgebra_glm::TMat4;
+
+#[derive(Debug, Clone)]
+pub struct VP {
+    pub view: TMat4<f32>,
+    pub projection: TMat4<f32>,
+}
+
+impl VP {
+    pub fn new() -> VP {
+        VP {
+            view: identity(),
+            projection: identity(),
+        }
+    }
+}
